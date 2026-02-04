3eafc7eb7fe9a4a9f35947ad01e347d2417706b7
alpawer <yarikbizyk@gmail.com>
2026-02-04 23:57:44 +0200

    initial PS1

---
+++ random_file.ps1
@@ -0,0 +1,36 @@
+$FolderPath = "./TestFiles"
+
+if (-not (Test-Path $FolderPath)) {
+    New-Item -ItemType Directory -Path $FolderPath | Out-Null
+    Write-Host "folder '$FolderPath' was create." -ForegroundColor Cyan 
+}
+
+Get-ChildItem -Path $FolderPath -File | Remove-Item 
+Write-Host "Folder is clear" -ForegroundColor Cyan
+Write-Host ""
+
+$RandomCount = Get-Random -Minimum 1 -Maximum 6
+
+Write-Host "Generating $RandomCount files" -ForegroundColor Yellow
+Write-Host ""
+ 
+for ($i = 1; $i -le $RandomCount; $i++) {
+    $FileName = "file_$i.txt"
+    $FilePath = Join-Path -Path $FolderPath -ChildPath $FileName
+    Set-Content -Path $FilePath -Value "Content number $i"
+    Write-Host "   made: $FileName" -ForegroundColor Green
+} 
+
+Write-Host ""
+
+$FileCount = (Get-ChildItem -Path $FolderPath -File | Measure-Object).Count
+
+Write-Host "We found $FileCount files" -ForegroundColor Cyan
+
+if ($FileCount -eq 3) {
+    Write-Host "-------------------" -ForegroundColor Magenta
+    Write-Host "Ура Київ за три дні" -ForegroundColor Magenta
+    Write-Host "-------------------" -ForegroundColor Magenta
+} else {
+    Write-Host "Amount of files is not equal to 3. I am sorry" -ForegroundColor Red
+}
